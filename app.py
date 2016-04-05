7c3db592d67de5b7ece8ed014d7b7f5ebde16d8b
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-05 17:34:48 -0400

    minor changes and shit

--- app.py
+++ app.py
@@ -3,15 +3,21 @@
 # Author: alvin.lin.dev@gmail.com (Alvin Lin)
 
 from flask import Flask
-from flask import request, render_template
+from flask import redirect, request
+
+from lib.api_accessor import ApiAccessor
 
 import sys
 
 app = Flask(__name__)
+api_accessor = ApiAccessor.create()
 
 @app.route('/')
-def index():
-    return '\033[01;31mHi\n'
+@app.route('/<section>', methods=['GET'])
+def index(section='home'):
+    if 'curl' in str(request.user_agent):
+        return str(api_accessor.fetch(section=section))
+    return redirect('http://www.nytimes.com/pages/%s' % section, code=302)
 
 if __name__ == '__main__':
     app.debug = '--debug' in sys.argv
