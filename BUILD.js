9a410b926c5f0f817db73401dc7e1f21d31d05bf
omgimanerd <alvin.lin.dev@gmail.com>
2016-12-16 16:32:33 -0500

    update css

--- BUILD.js
+++ BUILD.js
@@ -1,11 +1,10 @@
 /**
  * This file contains compilation and build rules for the project. This file
  * is imported by the gulpfile during compilation and build.
- * For build system: 2.0.0
  */
 
 module.exports = {
-  GULPFILE_VERSION: "2.0.0",
+  GULPFILE_VERSION: "2.1.0",
   DEFAULT_TASKS: ['js-lint'],
   JS_LINT_RULES: [
     {
