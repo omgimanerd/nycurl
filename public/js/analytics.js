96280c43aae2b3f80df43cf623be7cb5472c6589
omgimanerd <alvin.lin.dev@gmail.com>
2017-05-17 00:10:37 -0400

    removed google charts

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -2,10 +2,3 @@
  * @fileoverview Client side JavaScript for rendering site analytics.
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
-
-google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(function() {
-  $.post('/analytics', function(data) {
-    console.log(data);
-  });
-});
