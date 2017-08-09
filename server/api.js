1907578e342c57fecb1b7c8381ca1d25d6eea608
omgimanerd <alvin.lin.dev@gmail.com>
2017-08-09 01:59:40 -0700

    Fixed issue where some articles returned from the NYTimes API have no short_url field

--- server/api.js
+++ server/api.js
@@ -12,11 +12,23 @@ if (!NYTIMES_API_KEY) {
       NYTIMES_API_KEY in your environment variables.')
 }
 
+const URL_SHORTENER_API_KEY = process.env.URL_SHORTENER_API_KEY
+if (!URL_SHORTENER_API_KEY) {
+  throw new Error('No URL Shortener API key specified. Make sure you have \
+      URL_SHORTENER_API_KEY in your environment variables.')
+}
+
 /**
  * Base URL for the NYTimes API.
  * @type {string}
  */
 const NYTIMES_URL = 'http://api.nytimes.com/svc/topstories/v2'
+
+/**
+ * Base URL for the URL Shortener API.
+ * @type {type}
+ */
+const URL_SHORTENER_BASE_URL = 'https://www.googleapis.com/urlshortener/v1/url'
 
 /**
  * Milliseconds in 10 minutes, the duration which results will be cached.
@@ -46,6 +58,29 @@ const isValidSection = section => {
 }
 
 /**
+ * This method sends a request to Google's URL Shortener API to get
+ * a shortened URL and returns a Promise.
+ * @param {string} url The URL to shorten.
+ * @return {Promise}
+ */
+const shortenUrl = (url, callback) => {
+  return request({
+    uri: URL_SHORTENER_BASE_URL,
+    method: 'POST',
+    headers: {
+      // The Referer field is necessary because of the referrer limitation set
+      // on the production API key.
+      'Referer': 'nycurl.sytes.net',
+      'Content-Type': 'application/json'
+    },
+    body: { longUrl: url },
+    qs: { key: URL_SHORTENER_API_KEY },
+    json: true
+  }).then(data => data.id)
+    .catch(error => errorBuilder.promise('URLShortenerAPIError', error))
+}
+
+/**
  * This method fetches article data from the NY Times. It operates under the
  * assumption that the section being passed to it is a valid section to query
  * and that the isValidSection() check has passed.
@@ -53,7 +88,7 @@ const isValidSection = section => {
  * @return {Promise}
  */
 const fetchArticles = section => {
-  /**
+  /*
    * We first check if the section query has been cached within the last 10
    * minutes. If it has, then we return the cached data. If not, we then
    * fetch new data from the New York Times API.
@@ -63,7 +98,7 @@ const fetchArticles = section => {
     return Promise.resolve(cache[section].results)
   }
 
-  /**
+  /*
    * If the section being requested was not cached, then we need to fetch the
    * data from the New York Times. We will cache it before returning the
    * Promise.
@@ -73,11 +108,23 @@ const fetchArticles = section => {
     qs: { 'api-key': NYTIMES_API_KEY },
     json: true
   }).then(data => {
-    const results = data.results.sort((a, b) => {
-      return a.section.localeCompare(b.section)
-    })
-    /**
-     * We cache the result and then return it in a resolved Promise.
+    /*
+     * Some articles won't have a NYTimes short_url field, so we will shorten
+     * the article URL ourselves.
+     */
+    return Promise.all(data.results.map(article => {
+      if (article.short_url) {
+        return article
+      }
+      return shortenUrl(article.url).then(shortenedUrl => {
+        article.short_url = shortenedUrl
+        return article
+      })
+    }))
+  }).then(data => {
+    const results = data.sort((a, b) => a.section.localeCompare(b.section))
+    /*
+     * We cache the result and then return it through the Promise.
      */
     cache[section] = {
       results: results,
